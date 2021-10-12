84df4af7b2c06502535ccb13053eb0ee337ed3de
Evan <44183747+66chenbiao@users.noreply.github.com>
2021-10-12 18:08:54 +0800

    更换模板

--- lib/utils/ui_util.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-abstract class UIUtil {
-  static final webStyleWidth = 768;
-
-  static isPhoneStyle(BuildContext context) {
-    return MediaQuery.of(context).size.shortestSide < webStyleWidth;
-  }
-}
